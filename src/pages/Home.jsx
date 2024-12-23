5325a0337ebbfa37f71672659a5381fdceadff44
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-12-23 18:04:56 +0530

    Add powerful yet simple section in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
-import { FaChartPie, FaStar, FaRegLightbulb } from "react-icons/fa";
-
+import { FaChartPie, FaStar, FaRegLightbulb, FaRegChartBar, FaRoad } from "react-icons/fa";
+import { IoIosLock } from "react-icons/io";
 import banner1 from '../assets/banner1.jpg'
 import featureImg from '../assets/img1.png'
+import alleyGrey from '../assets/alleyGreybg.jpg'
+import img2 from '../assets/img2.png'
 import FreeTrailForm from '../components/FreeTrailForm'
 
 
@@ -54,6 +56,40 @@ const Home = () => {
             </div>
             <div>
               <img className='w-[360px] md:w-420 lg:w-[470px]' src={featureImg} alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="w-full bg-cover bg-center bg-fixed flex items-center justify-center p-10" style={{backgroundImage: `url(${alleyGrey})`}}>
+        <div className='max-w-7xl mx-auto flex flex-col gap-5'>
+          <h1 className='sm:text-3xl text-2xl mb-3 text-white text-center font-medium'>Powerful yet simple</h1>
+          <div className='flex md:flex-row flex-col-reverse items-center md:items-start md:justify-between gap-2 md:gap-6'>
+            <div>
+              <img className='w-[360px] md:w-400 lg:w-[450px]' src={featureImg} alt="" />
+            </div>
+            <div className='flex flex-col gap-5 max-w-96'>
+                <div className='flex flex-row items-start gap-4'>
+                  <div className='text-white mt-2'><FaRegChartBar size={30}/></div>
+                  <div className='flex flex-col gap-2 text-white'>
+                    <h3 className='text-xl font-medium'>Full reporting and visibilit</h3>
+                    <p className='text-xs'>All the analysis tools you’ll need to better nurture and align talent with business needs.</p>
+                  </div>
+                </div>
+                <div className='flex flex-row items-start gap-4'>
+                  <div className='text-white mt-2'><IoIosLock size={30}/></div>
+                  <div className='flex flex-col gap-2 text-white'>
+                    <h3 className='text-xl font-medium'>Secure and reliable</h3>
+                    <p className='text-xs'>Gain total peace of mind with a single, secure storage place for all your HR documents.</p>
+                  </div>
+                </div>
+                <div className='flex flex-row items-start gap-4'>
+                  <div className='text-white mt-2'><FaRoad size={30}/></div>
+                  <div className='flex flex-col gap-2 text-white'>
+                    <h3 className='text-xl font-medium'>Your HR, your way</h3>
+                    <p className='text-xs'>The perfect fit for your growing organisation.</p>
+                  </div>
+                </div>
             </div>
           </div>
         </div>
