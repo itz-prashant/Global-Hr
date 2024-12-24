d4a59244a1fcf4b6d6a21a0e018b8bfeb47705f3
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-12-24 11:51:59 +0530

    Add banner section in tour page

--- src/pages/Tour.jsx
+++ src/pages/Tour.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import tourBanner from '../assets/tourBanner.jpg'
+import Button from '../components/Button'
+import { Link } from 'react-router-dom'
 
 const Tour = () => {
   return (
-    <div>Tour</div>
+    <>
+      <div className="w-full mt-20 bg-cover bg-center md:h-[80vh] h-full flex items-center justify-center" style={{backgroundImage: `url(${tourBanner})`}}>
+        <div className='max-w-7xl mx-auto w-full py-10 px-4 md:px-10 flex items-start'>
+          <div className='md:w-[490px] lg:w-[550px] sm:w-80 w-60 flex flex-col gap-3 items-center'>
+            <h1 className='md:text-5xl text-3xl text-white text-center'>Take a tour of Global HR.</h1>
+            <p className='sm:text-xl md:text-2xl text-white md:mb-10 mb-5 text-center'>Check out or easy to use HR software features for yourself!</p>
+            <Link to={"/tour"}>
+              <Button background={"blue"} text="Take a tour"/>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
