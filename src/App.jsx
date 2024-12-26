f8beadc800be98bef52236b72a6820169448fd3d
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-12-26 17:33:49 +0530

    Add login register button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Link, Route, Routes } from "react-router-dom"
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -8,12 +8,19 @@ import Pricing from './pages/Pricing'
 import WhoWeAre from './pages/WhoWeAre'
 import ReachUs from './pages/ReachUs'
 import WebLogin from './pages/WebLogin'
+import logIn from './assets/log-in.png'
+import register from './assets/Register.png'
+
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className="relative">
+        <div className='fixed flex flex-col gap-3 right-0 top-1/2 -translate-y-1/2'>
+          <Link to={"https://globalhruk.com/globalhr/"}><img  className="w-[32px]" src={logIn} alt="" /></Link>
+          <Link to={"https://globalhruk.com/globalhr/index.php/login/user_registration_c/index"}><img  className="w-[32px]" src={register} alt="" /></Link>
+        </div>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />}/>
