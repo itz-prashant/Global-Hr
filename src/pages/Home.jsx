3347e216faf7535448a439a6c07dfe0a36c5cd85
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-12-27 11:46:43 +0530

    Add animation in banner sectionhome page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { motion } from "motion/react"
 import { FaChartPie, FaStar, FaRegLightbulb, FaRegChartBar, FaRoad } from "react-icons/fa";
 import { IoIosLock } from "react-icons/io";
 import banner1 from '../assets/banner1.jpg'
@@ -12,18 +13,39 @@ const Home = () => {
 
   const [errorMessageBox, setErrorMessageBox] = useState(false)
 
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.3, // Delay between children animations
+      },
+    },
+  };
+
+  const childVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  };
+
+  useEffect(()=>{
+      window.scrollTo(0,0)
+    },[])
+
   return (
     <>
-      <div className="w-full mt-20 bg-cover bg-center md:h-[80vh] h-full flex items-center justify-center" style={{backgroundImage: `url(${banner1})`}}>
+      <div className="w-full mt-20 bg-cover bg-center md:h-[90vh] h-full flex items-center justify-center" style={{backgroundImage: `url(${banner1})`}}>
 
-        <div className='max-w-7xl mx-auto md:w-full sm:w-[470px] w-96 flex flex-col px-5 py-10 md:py-0 items-center sm:gap-5 gap-3'>
-          <h1 className='md:text-5xl text-4xl text-white text-center'>Make HR Process Quick, Easy and Simple.</h1>
-          <p className='sm:text-2xl text-lg text-white md:mb-10 mb-5 text-center'>HR Software for companies where people matter</p>
+        <motion.div variants={containerVariants} initial="hidden" animate="visible" className='max-w-7xl mx-auto md:w-full sm:w-[470px] w-96 flex flex-col px-5 py-10 md:py-0 items-center sm:gap-5 gap-3'>
+          <motion.h1 variants={childVariants} className='md:text-5xl text-4xl text-white text-center'>Make HR Process Quick, Easy and Simple.</motion.h1>
+          <motion.p variants={childVariants} className='sm:text-2xl text-lg text-white md:mb-10 mb-5 text-center'>HR Software for companies where people matter</motion.p>
           {errorMessageBox && <div className='bg-red-100 p-5 rounded'>
           <p className='text-red-600 text-xs text-center'>There was a problem with your submission. Errors are marked below. </p>
           </div>}
-          <FreeTrailForm direction="row" text="Start Free Trial" transparent="white" background="yellow" setErrorMessageBox={setErrorMessageBox}/>
-        </div>
+          <motion.div variants={childVariants}>
+            <FreeTrailForm direction="row" text="Start Free Trial" transparent="white" background="yellow" setErrorMessageBox={setErrorMessageBox}/>
+          </motion.div>
+        </motion.div>
       </div>
       
       <section className='w-full p-10 bg-[#23a7df] flex items-center justify-center'>
