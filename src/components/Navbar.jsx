a02ab5fca2229827af24116bcac9733559afba02
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-12-28 18:00:45 +0530

    Add custom media query and fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <header className='flex flex-col w-full min-h-20 px-8 fixed top-0 bg-white'>
-      <div className={`flex items-center w-full max-w-7xl px-5 absolute left-1/2 -translate-x-1/2 justify-between ${handelMenu ? "border-b-[0.5px] border-gray-200" : ""}`}>
+      <div className={`flex items-center xl:w-[1140px] md:w-[720px] lg:w-[940px] w-[300px] xs:w-[390px] flex-shrink absolute left-1/2 -translate-x-1/2 justify-between ${handelMenu ? "border-b-[0.5px] border-gray-200" : ""}`}>
         <img src={logo} className='max-w-36 max-h-20' alt="" />
         <nav className='hidden md:block'>
           <ul className='flex items-center gap-6 text-black/80 text-sm'>
@@ -25,8 +25,8 @@ const Navbar = () => {
         </nav>
         <IoMenu size={32} onClick={()=> setHandleMenu(!handelMenu)} className='md:hidden text-gray-500 cursor-pointer'/>
       </div>
-      {handelMenu && <nav className='md:hidden mt-20'>
-        <ul className='flex flex-col gap-6 text-black/80 text-sm p-4'>
+      {handelMenu && <nav className='md:hidden mt-20 w-full'>
+        <ul className='flex flex-col gap-6 text-black/80 w-[300px] xs:w-[390] mx-auto text-sm py-4'>
             <Link to={"/"} onClick={()=>setHandleMenu(!handelMenu)} className='hover:text-blue-600'>Home</Link>
             <Link to={"/tour"} onClick={()=>setHandleMenu(!handelMenu)} className='hover:text-blue-600'>Tour</Link>
             <Link to={"/showcase"} onClick={()=>setHandleMenu(!handelMenu)} className='hover:text-blue-600'>Showcase</Link>
