aa7c9cadab4b32e107d03287fd2f1d6aca4effd1
Prashant Gupta <prashantguptaofficial007@gmail.com>
2024-12-29 14:52:09 +0530

    Fix showcase page style

--- src/components/CenterCard.jsx
+++ src/components/CenterCard.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const CenterCard = ({image, name}) => {
   return (
-    <div className='sm:w-56 w-48'>
-        <div className='bg-[#da5427] flex items-center justify-center'>
+    <div className='flex flex-col'>
+        <div className='bg-[#da5427] h-[120px] flex items-center justify-center'>
             <img src={image} alt="" />
         </div>
-        <div className='bg-black text-white text-center py-3'>{name}</div>
+        <div className='bg-black text-white text-center xs:h-[60px] h-fit font-semibold py-3'>{name}</div>
     </div>
   )
 }
--- src/pages/Showcase.jsx
+++ src/pages/Showcase.jsx
@@ -45,11 +45,11 @@ const Showcase = () => {
 
   return (
     <>
-      <div className="w-full mt-20 bg-cover bg-center sm:h-[80vh] h-full flex items-center justify-center" style={{backgroundImage: `url(${showcaseBanner})`}}>
-        <div className='max-w-7xl mx-auto w-full py-10 px-4 md:px-10 flex items-center justify-center'>
+      <div className="w-full mt-20 bg-cover bg-center py-5 xs:h-[90vh] flex items-center justify-center" style={{backgroundImage: `url(${showcaseBanner})`}}>
+        <div className='xl:w-[1140px] md:w-[720px] lg:w-[940px] w-[300px] xs:w-[390px] py-10 px-4 md:px-10 flex items-center justify-center'>
           <div className='flex flex-col gap-3 items-center md:w-[600px] w-[450px]'>
-            <h1 className='md:text-5xl text-3xl text-white text-center'>Global HR : Cloud based complete HR Solution</h1>
-            <p className='sm:text-xl md:text-2xl text-white md:mb-10 mb-5 text-center'>Everything a company needs to manage its most important asset its Human Resource</p>
+            <h1 className='text-[34px] xs:text-[56px] xs:text text-white text-center'>Global HR : Cloud based complete HR Solution</h1>
+            <p className='text-xl xs:text-[26px] text-white md:mb-10 mb-5 text-center'>Everything a company needs to manage its most important asset its Human Resource</p>
             <Link to={"/showcase"}>
               <Button background={"blue"} text="Product Overview"/>
             </Link>
@@ -57,51 +57,51 @@ const Showcase = () => {
         </div>
       </div>
       <section className='w-full sm:p-10 p-5 flex items-center justify-center'>
-          <div className='max-w-7xl mx-auto md:w-full w-96 sm:w-[450px] flex flex-col gap-4'>
-              <h1 className='sm:text-3xl text-xl text-black/65 text-center font-medium'>Offering one stop HR solution for small and medium businesses.</h1>
+          <div className='xl:w-[1140px] md:w-[720px] lg:w-[940px] w-[300px] xs:w-[390px] flex flex-col gap-4 px-4 py-10'>
+              <h1 className='text-[26px] leading-[95%] text-black/70 text-center font-semibold'>Offering one stop HR solution for small and medium businesses.</h1>
               <p className='text-[15px] text-black/65 md:mb-10 mb-5 text-center'>Customize Global HR to suit your needs and reflect your brand.</p>
               <motion.div variants={containerVariants} ref={ref} initial="hidden" animate={controls} className='grid md:grid-cols-2 grid-cols-1 gap-5 justify-between'>
                 <motion.div variants={childVariants} className='flex flex-row items-start gap-5'>
                   <div className='text-[#045089]'><IoMdGitCompare size={32}/></div>
                   <div className='flex flex-col gap-2'>
-                      <h3 className='text-2xl font-medium text-black/70'>System Implementation</h3>
-                      <p className=' text-black/70 text-sm'>Our experts will ensure People is up and running smoothly for your business from day one, so you can start making impact right away.</p>
+                      <h3 className='text-[22px] font-medium text-black/70'>System Implementation</h3>
+                      <p className=' text-black/70 text-[15px]'>Our experts will ensure People is up and running smoothly for your business from day one, so you can start making impact right away.</p>
                   </div>
                 </motion.div>
                 <motion.div variants={childVariants} className='flex flex-row items-start gap-5'>
                   <div className='text-[#045089]'><FaRandom size={32}/></div>
                   <div className='flex flex-col gap-2'>
-                    <h3 className='text-2xl font-medium text-black/70'>Free Switching Service</h3>
-                    <p className='text-black/70 text-sm'>Our Free Switching Service makes it easy to migrate your HR data from your old system over to People, without any added hassle.</p>
+                    <h3 className='text-[22px] font-medium text-black/70'>Free Switching Service</h3>
+                    <p className='text-black/70 text-[15px]'>Our Free Switching Service makes it easy to migrate your HR data from your old system over to People, without any added hassle.</p>
                   </div>
                 </motion.div>
                 <motion.div variants={childVariants} className='flex flex-row items-start gap-5'>
                   <div className='text-[#045089]'><MdGroups size={36}/></div>
                   <div className='flex flex-col gap-2'>
-                      <h3 className='text-2xl font-medium text-black/70'>Online or On-Site Training</h3>
-                      <p className=' text-black/70 text-sm'>If you want to get your whole workforce up to speed with People at the same time, our experts can deliver training for your workforce either online or on-site.</p>
+                      <h3 className='text-[22px] font-medium text-black/70'>Online or On-Site Training</h3>
+                      <p className=' text-black/70 text-[15px]'>If you want to get your whole workforce up to speed with People at the same time, our experts can deliver training for your workforce either online or on-site.</p>
                   </div>
                 </motion.div>
                 <motion.div variants={childVariants} className='flex flex-row items-start gap-5'>
                   <div className='text-[#045089]'><FaQuestionCircle size={36}/></div>
                   <div className='flex flex-col gap-2'>
-                    <h3 className='text-2xl font-medium text-black/70'>Full service support desk</h3>
-                    <p className='text-black/70 text-sm'>Our office hours (9am – 5pm) make it easy to find instant help, even if you have offices outside the UK.</p>
+                    <h3 className='text-[22px] font-medium text-black/70'>Full service support desk</h3>
+                    <p className='text-black/70 text-[15px]'>Our office hours (9am – 5pm) make it easy to find instant help, even if you have offices outside the UK.</p>
                   </div>
                 </motion.div>
               </motion.div>
           </div>
       </section>
       <section className='w-full sm:p-10 p-5 flex items-center justify-center'>
-          <div className='max-w-7xl mx-auto md:w-full w-[400px] sm:w-[500px] flex flex-col items-center gap-4'>
-              <h1 className='sm:text-3xl text-xl text-black/65 text-center font-medium'>Work Smarter, Work Better.</h1>
-              <p className='text-[15px] text-black/65 md:mb-10 mb-5 '>
+          <div className='xl:w-[1140px] md:w-[720px] lg:w-[940px] w-[300px] xs:w-[390px] mx-auto flex flex-col items-center gap-4 px-4'>
+              <h1 className='text-[26px] text-black/70 font-semibold '>Work Smarter, Work Better.</h1>
+              <p className='text-[15px] text-black/65 md:mb-10 mb-5 xl:w-[819px] md:w-[525px] lg:w-[769px] w-[270px] xs:w-[360px]'>
                 GlobalHR is a cloud-based Human Resources soluton that provides businesses with an easy way to arrange their day-to-day HR needs.
                 <br />
                 <br />
                 We understand that for many companies, managing their HR with spreadsheets can sometimes get in the way of the important stuff, like growing their business.
                 </p>
-              <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 justify-evenly grid-cols-1 gap-6'> 
+              <div className='grid lg:grid-cols-4 md:grid-cols-3 xs:grid-cols-2 justify-evenly grid-cols-1 gap-5'> 
                   {
                     cardData.map((data)=>(
                       <CenterCard key={data.name} image={data.image} name={data.name}/>
